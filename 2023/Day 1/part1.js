6b7360976e2ea781d5ec8874ab680ff5ad8245b2
Aylee Andersen <aylee@Aylees-MacBook-Pro.local>
2023-11-30 09:18:14 -0700

    Modified template to work with my setup

--- 2023/Day 1/part1.js
+++ 2023/Day 1/part1.js
@@ -1,12 +1,12 @@
 const day = 1;
 const use_example = true;
 
-const filename = '2023/Day ' + day + '/' + (use_example ? 'example' : 'input') + '.txt';
+const filename = 'Advent-of-code/2023/Day ' + day + '/' + (use_example ? 'example' : 'input') + '.txt';
 const fs = require('fs');
 let og_input = fs.readFileSync(filename, 'utf-8');
 let input_array = og_input.split('\n');
 
 //part 1
-for (let i in input_array){
-    
+for (let i = 0; i < input_array.length; i++){
+  let item = input_array[i] == "" ? "" : parseInt(input_array[i]);
 }
