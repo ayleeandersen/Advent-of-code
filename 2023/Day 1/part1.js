fb06067f0ab4858953a04e1c9119956d2550f4f1
Aylee Andersen <aylee@Aylees-MacBook-Pro.local>
2023-11-30 23:03:54 -0700

    Finished day 1

--- 2023/Day 1/part1.js
+++ 2023/Day 1/part1.js
@@ -6,7 +6,55 @@ const fs = require('fs');
 let og_input = fs.readFileSync(filename, 'utf-8');
 let input_array = og_input.split('\n');
 
-//part 1
-for (let i = 0; i < input_array.length; i++){
-  let item = input_array[i] == "" ? "" : parseInt(input_array[i]);
+const digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let sum = 0;
+for (let i = 0; i < input_array.length; i++) {
+  let item = input_array[i];
+  let smallSum = "";
+
+  let firstIndex = item.length - 1;
+  let firstVal = '';
+  let replace = false;
+  // First digit
+  for (let j = 0; j < digits.length; j++) {
+    let digit = digits[j];
+    let index = item.indexOf(digit);
+    if (index != -1 && index < firstIndex) {
+      firstIndex = index;
+      firstVal = digit;
+      replace = true;
+    }
+  }
+  if (replace && firstVal.length > 1) {
+    item = item.replace(firstVal, "x" + firstVal.substring(1, -1));
+  } else if (replace && firstVal.length === 1) {
+    item = item.replace(firstVal, "x");
+  }
+  // Last digit
+  let lastIndex = 0;
+  let lastVal = '';
+  for (let j = 0; j < digits.length; j++) {
+    let digit = digits[j];
+    let index = item.indexOf(digit);
+    while (index != -1) {
+      if (item.indexOf(digit, index + 1) !== -1) {
+        index = item.indexOf(digit, index + 1);
+      } else {
+        break;
+      }
+    }
+    if (index != -1 && index > lastIndex) {
+      lastIndex = index;
+      lastVal = digit;
+    }
+  }
+
+  // Sum
+  smallSum = (firstVal) + (lastVal);
+  if (smallSum.length === 1) {
+    smallSum = smallSum + smallSum;
+  }
+  sum += parseInt(smallSum);
 }
+
+console.log("SUM", sum);
---
+++ 2023/Day 2/part2.js
@@ -0,0 +1,14 @@
+const day = 2;
+const use_example = true;
+
+const filename = 'Advent-of-code/2023/Day ' + day + '/' + (use_example ? 'example' : 'input') + '.txt';
+const fs = require('fs');
+let og_input = fs.readFileSync(filename, 'utf-8');
+let input_array = og_input.split('\n');
+
+// PART 2
+for (let i = 0; i < input_array.length; i++) {
+
+}
+
+console.log("ANSWER");
