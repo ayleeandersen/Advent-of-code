8c3a17ec3c57dfa8f2e810bc6b2263533ebe63e8
Aylee Andersen <aylee@Aylees-MacBook-Pro.local>
2023-12-04 09:10:09 -0700

    Completed Day 4

--- 2023/Day 4/part1.js
+++ 2023/Day 4/part1.js
@@ -9,6 +9,19 @@ let input_array = og_input.split('\n');
 // PART 1
 let sum = 0;
 for (let i = 0; i < input_array.length; i++) {
+  let both = input_array[i].replace(/Card.*\: /g, "");
+  let bothArr = both.split("|");
+  let winningValues = bothArr[0].split(" ").filter(val => val.length > 0);
+  let yourValues = bothArr[1].split(" ").filter(val => val.length > 0);
+  let rowScore = 0;
+  for (let j = 0; j < yourValues.length; j++) {
+    let yourValue = yourValues[j];
+    if (winningValues.indexOf(yourValue) !== -1) {
+      if (rowScore == 0) rowScore = 1;
+      else rowScore = rowScore * 2;
+    }
+  }
+  sum += rowScore;
 }
 
 console.log("ANSWER", sum);
--- 2023/Day 4/part2.js
+++ 2023/Day 4/part2.js
@@ -8,7 +8,38 @@ let input_array = og_input.split('\n');
 
 // PART 2
 let sum = 0;
+let cardCopies = {};
 for (let i = 0; i < input_array.length; i++) {
+  cardCopies[i+1] = 1;
+}
+for (let i = 0; i < input_array.length; i++) {
+  let cardNum = i+1;
+  // console.log("Card Num", cardNum);
+  // console.log("Card Copies", cardCopies[cardNum]);
+  let both = input_array[i].replace(/Card.*\: /g, "");
+  let bothArr = both.split("|");
+  let winningValues = bothArr[0].split(" ").filter(val => val.length > 0);
+  // console.log("Winning values", winningValues);
+  let yourValues = bothArr[1].split(" ").filter(val => val.length > 0);
+  // console.log("Your values", yourValues);
+  for (let z = 0; z < cardCopies[cardNum]; z++) {
+    let matches = 0;
+    for (let j = 0; j < yourValues.length; j++) {
+      let yourValue = yourValues[j];
+      if (winningValues.indexOf(yourValue) !== -1) {
+        // console.log("Match!", yourValue);
+        matches++;
+      }
+    }
+    for (let k = 0; k < matches; k++) {
+      cardCopies[cardNum+k+1] = cardCopies[cardNum+k+1] + 1;
+      // console.log("Card Copies added", cardCopies);
+    }
+  }
+}
+
+for (let card in cardCopies) {
+  sum += cardCopies[card];
 }
 
 console.log("ANSWER", sum);
