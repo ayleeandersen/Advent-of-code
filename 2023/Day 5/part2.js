aca628be89eabee13b0e27a6215fa9b47c095851
Aylee Andersen <aylee@Aylees-MacBook-Pro.local>
2023-12-06 14:36:14 -0700

    Added better code comments

--- 2023/Day 5/part2.js
+++ 2023/Day 5/part2.js
@@ -7,7 +7,10 @@ let og_input = fs.readFileSync(filename, 'utf-8');
 let input_array = og_input.split('\n');
 input_array = input_array.filter(val => val.length > 0);
 
-// PART 2 - Wrong answers: 34062605 (too high), 23043868 (too low)
+// PART 2
+// Wrong answers: 34062605 (too high), 23043868 (too low)
+
+// Take the row of seeds and create an array of objects containing information about each range of seeds
 let seedsLine = input_array[0].replace("seeds: ", "");
 let seedVals = seedsLine.split(" ");
 let seedGroups = [];
@@ -25,6 +28,7 @@ for (let i = 0; i < seedVals.length; i+=2) {
 // seedGroups.sort((groupA, groupB) => groupA.start - groupB.start);
 // console.log("SEED GROUPS", seedGroups);
 
+// Loop through each set of conversions and store their source and destination range information
 let lowest = 99999999999999999n;
 let maps = {};
 let currentConversionType = "";
@@ -44,9 +48,10 @@ for (let i = 1; i < input_array.length; i++) {
     maps[currentConversionType].sort((groupA, groupB) => groupA.sourceStart - groupB.sourceStart);
   }
 }
-
 // console.log("MAPS", maps);
 
+// Loop through each range of seeds and go through the conversion process for each range
+// Note: more seed ranges will be added to this list when a range has to be split in two
 for (let s = 0; s < seedGroups.length; s++) {
   let seed = seedGroups[s];
   let mIndex = 0;
@@ -54,7 +59,8 @@ for (let s = 0; s < seedGroups.length; s++) {
     let map = maps[mKey];
     for (let m = 0; m < map.length; m++) {
       let mapRow = map[m];
-      // If the range starts in the map row or if the range ends in the map row, we want to jump into it
+      // If the range starts in the map row or if the range ends in the map row, we want to perform the conversion
+      // Also, if it was a seed range that was added later, we want to make sure we don't do conversions on it until it reaches the conversion set of maps that it was split up at
       if (seed.startOnMap <= mIndex && ((seed.end >= mapRow.sourceStart && seed.end <= mapRow.sourceEnd) || (seed.start >= mapRow.sourceStart && seed.start <= mapRow.sourceEnd))) {
         // console.log("Seed", seed);
         // console.log("Map Row", mapRow);
@@ -62,7 +68,7 @@ for (let s = 0; s < seedGroups.length; s++) {
         let diffEnd = mapRow.range - ((mapRow.sourceEnd + mapRow.range) - seed.end);
         seed.convertedValueStart = mapRow.destStart + diffStart;
         seed.convertedValueEnd = mapRow.destEnd + diffEnd;
-        // Trim off the ends as a new seed range
+        // Trim off the end if it doesn't fit in the range and push it to the back of the list of seed ranges
         if (seed.start < mapRow.sourceStart) {
           seed.convertedValueStart = mapRow.destStart;
           seedGroups.push({
@@ -76,6 +82,7 @@ for (let s = 0; s < seedGroups.length; s++) {
           });
           // console.log("Added New Seed", seedGroups[seedGroups.length-1]);
         }
+        // Trim off the other end that doesn't fit in the range and push it to the back of the list of seed ranges
         if (seed.end > mapRow.sourceEnd) {
           seed.convertedValueEnd = mapRow.destEnd;
           seedGroups.push({
@@ -91,9 +98,11 @@ for (let s = 0; s < seedGroups.length; s++) {
           // console.log("Added New Seed", seedGroups[seedGroups.length-1]);
         }
         // console.log("Updated Seed", seed);
+        // Once a conversion happens, skip ahead to the next set of conversion maps
         break;
       }
     }
+    // Update the current seed range so that the converted values are the new start and end of the range
     seed = {
       id: seed.id,
       start: seed.convertedValueStart,
@@ -108,6 +117,7 @@ for (let s = 0; s < seedGroups.length; s++) {
   // console.log("FINAL SEED VALUE", seed);
 }
 
+// Update all the seed ranges so that the converted values are the new start and end of the range (should already be the case but just in case)
 seedGroups = seedGroups.map(group => ({
   id: group.id,
   start: group.convertedValueStart,
@@ -117,6 +127,7 @@ seedGroups = seedGroups.map(group => ({
   range: group.range
 }));
 
+// Grab the smallest number in the range
 for (let sKey in seedGroups) {
   let seed = seedGroups[sKey];
   // console.log("SEED", seed);
